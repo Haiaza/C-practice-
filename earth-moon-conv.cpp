c3b9d3f8052b16f0e1a162ec91038d7462c62b5c
Haiaza <83097184+Haiaza@users.noreply.github.com>
2025-07-22 15:46:38 +0000

    Fully fleshed out, removing unused variable

--- earth-moon-conv.cpp
+++ earth-moon-conv.cpp
@@ -1,14 +1,25 @@
-//earth pounds
+/*
+This program is meant to convert weight on Earth to its Moon equivalent
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //earth pounds
 int ep;
 //moon pounds
 double mp;
 //current count
-int cc;
+// int cc;
 
 for(ep=1; ep<=100; ep++){
-    mp = .17 * ep
+    mp = .17 * ep;
     cout << "Earth weight : " << ep << " is the same as moon weight : " << mp << "\n";
     if(ep % 25 == 0){
-        cout << "\n"
+        cout << "\n";
     }  
+}
+return 0;
 }
