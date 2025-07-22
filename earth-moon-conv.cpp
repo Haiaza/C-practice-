60afcb2d44342da1717fa51d8b10535cae9dec20
Haiaza <83097184+Haiaza@users.noreply.github.com>
2025-07-22 15:44:22 +0000

    Logic outline for the conversion

---
+++ earth-moon-conv.cpp
@@ -0,0 +1,14 @@
+//earth pounds
+int ep;
+//moon pounds
+double mp;
+//current count
+int cc;
+
+for(ep=1; ep<=100; ep++){
+    mp = .17 * ep
+    cout << "Earth weight : " << ep << " is the same as moon weight : " << mp << "\n";
+    if(ep % 25 == 0){
+        cout << "\n"
+    }  
+}
